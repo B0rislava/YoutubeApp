80513d61fa34ba7d15f19ee04dfb3cd2c1279fc5
B0rislava <borislava.g.ivanova.2021@elsys-bg.org>
2025-08-28 11:00:45 +0300

    Adding HomeScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,9 @@ android {
 
 dependencies {
 
+    implementation("com.pierfrancescosoffritti.androidyoutubeplayer:core:12.1.0")
+    implementation("com.github.skydoves:landscapist-glide:2.3.3")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
